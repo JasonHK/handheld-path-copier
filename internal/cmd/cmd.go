5a40c8aa548f18bfef1a8884598d3da6701b9cdc
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2026-09-24 16:40:52 +0800

    feat: add mousetrap for Windows Explorer on fatal error (#1)

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -12,6 +12,7 @@ import (
 	"github.com/JasonHK/handheld-path-copier/internal/locales"
 	"github.com/adrg/xdg"
 	"github.com/fsnotify/fsnotify"
+	"github.com/inconshreveable/mousetrap"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 	"github.com/spf13/cobra"
 	"golang.design/x/clipboard"
@@ -161,5 +162,13 @@ func run(cmd *cobra.Command, args []string) {
 
 func fatal(v ...any) {
 	fmt.Fprintln(os.Stderr, v...)
+	if mousetrap.StartedByExplorer() {
+		fmt.Println(locales.Localizer.MustLocalizeMessage(&i18n.Message{
+			ID:    "message_press_enter",
+			Other: "Press Enter to continue...",
+		}))
+		fmt.Scanln()
+	}
+
 	os.Exit(1)
 }
